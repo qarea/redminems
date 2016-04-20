34442c718c412c4f9d246c4fe232031347c08bc9
Alex Efros <powerman@powerman.name>
2016-04-20 03:16:53 +0300

    more tests

--- api/websvc/remoteip_test.go
+++ api/websvc/remoteip_test.go
@@ -1,7 +1,15 @@
 package websvc
 
 import (
+	_ "log"
 	"net/http"
+	"os"
+	"os/exec"
+	"runtime"
+	"strings"
+	"syscall"
+
+	"github.com/powerman/narada-go/narada/staging"
 
 	. "gopkg.in/check.v1"
 
@@ -32,7 +40,37 @@ func (s *TestIP) Test(c *C) {
 	c.Check(remoteIP(r), Equals, "1.2.3.4")
 	cfg.HTTP.RealIPHeader = "X-Real-IP"
 	c.Check(remoteIP(r), Equals, "4.3.2.1")
-	cfg.HTTP.RealIPHeader = "X-Real-REMOTE_ADDR"
-	// TODO test log.Fatal using subprocess
-	// c.Check(func() { remoteIP(r) }, PanicMatches, ".*config/real_ip_header.*")
+}
+
+func (s *TestIP) TestCrasher(c *C) {
+	crasher(c, 1, ".*failed to detect remote IP.*\n", func() {
+		r, err := http.NewRequest("GET", "http://websvc.test/", nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+		r.RemoteAddr = "1.2.3.4:0"
+		r.Header.Set("X-Real-IP", "4.3.2.1")
+		cfg.HTTP.RealIPHeader = "X-Real-REMOTE_ADDR"
+		remoteIP(r)
+	})
+}
+
+func crasher(c *C, exitCode int, stderrRegex string, test func()) {
+	if os.Getenv("BE_CRASHER") == "1" {
+		test()
+		return
+	}
+	pc, _, _, _ := runtime.Caller(1)
+	parts := strings.Split(runtime.FuncForPC(pc).Name(), ".")
+	caller := parts[len(parts)-1]
+	cmd := exec.Command(os.Args[0], "-check.f=^"+caller+"$")
+	cmd.Dir = staging.BaseDir
+	cmd.Env = append(os.Environ(), "BE_CRASHER=1")
+	_, err := cmd.Output()
+	if e, ok := err.(*exec.ExitError); ok && !e.Success() {
+		c.Check(e.Sys().(syscall.WaitStatus).ExitStatus(), Equals, exitCode)
+		c.Check(string(e.Stderr), Matches, stderrRegex)
+	} else {
+		c.Errorf("process ran with err %v, want exit status 1", err)
+	}
 }
