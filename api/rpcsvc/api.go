a181961d38cfbb241bd2d868d628281685da35a2
Prylutskyi Anatolii <prylutskyi@qarea.com>
2016-10-06 13:46:30 +0300

    Add docs to rpcsvc package

--- api/rpcsvc/api.go
+++ api/rpcsvc/api.go
@@ -5,36 +5,43 @@ import (
 	"gitlab.qarea.org/tgms/ctxtg"
 )
 
+// ProjectsReq input parameter to GetProjects
 type ProjectsReq struct {
 	Context ctxtg.Context
 	Tracker entities.Tracker
 	entities.Pagination
 }
 
+// ProjectsResp output parameter from GetProjects
 type ProjectsResp struct {
 	Projects []entities.Project
 	Amount   int64
 }
 
+// ProjectDetailsReq input parameter to GetProjectDetails
 type ProjectDetailsReq struct {
 	Context   ctxtg.Context
 	Tracker   entities.Tracker
 	ProjectID entities.ProjectID
 }
 
+// ProjectDetailsResp output parameter from GetProjectDetails
 type ProjectDetailsResp struct {
 	Project entities.Project
 }
 
+// CurrentUserReq input parameter to GetCurrentUser
 type CurrentUserReq struct {
 	Context ctxtg.Context
 	Tracker entities.Tracker
 }
 
+// CurrentUserResp output parameter from GetCurrentUser
 type CurrentUserResp struct {
 	User entities.User
 }
 
+// ProjectIssuesReq input parameter to GetProjectIssues
 type ProjectIssuesReq struct {
 	Context   ctxtg.Context
 	Tracker   entities.Tracker
@@ -42,11 +49,13 @@ type ProjectIssuesReq struct {
 	entities.Pagination
 }
 
+// ProjectIssuesResp output parameter from GetProjectIssues
 type ProjectIssuesResp struct {
 	Issues []entities.Issue
 	Amount int64
 }
 
+// CreateIssueReq input parameter to CreateIssue
 type CreateIssueReq struct {
 	Context   ctxtg.Context
 	Tracker   entities.Tracker
@@ -54,10 +63,12 @@ type CreateIssueReq struct {
 	ProjectID entities.ProjectID
 }
 
+// CreateIssueResp output parameter from CreateIssue
 type CreateIssueResp struct {
 	Issue entities.Issue
 }
 
+// GetIssueReq input parameter to GetIssue
 type GetIssueReq struct {
 	Context   ctxtg.Context
 	Tracker   entities.Tracker
@@ -65,10 +76,12 @@ type GetIssueReq struct {
 	ProjectID entities.ProjectID
 }
 
+// GetIssueResp output parameter from GetIssue
 type GetIssueResp struct {
 	Issue entities.Issue
 }
 
+// CreateReportReq input parameter from CreateReport
 type CreateReportReq struct {
 	Context   ctxtg.Context
 	Tracker   entities.Tracker
@@ -76,17 +89,20 @@ type CreateReportReq struct {
 	Report    entities.Report
 }
 
+// GetIssueByURLReq input parameter to GetIssueByURL
 type GetIssueByURLReq struct {
 	Context  ctxtg.Context
 	Tracker  entities.Tracker
 	IssueURL entities.IssueURL
 }
 
+// GetIssueByURLResp output parameter from GetIssueByURL
 type GetIssueByURLResp struct {
 	Issue     entities.Issue
 	ProjectID entities.ProjectID
 }
 
+// UpdateIssueProgressReq input parameter to UpdateIssueReq
 type UpdateIssueProgressReq struct {
 	Context   ctxtg.Context
 	Tracker   entities.Tracker
@@ -95,12 +111,14 @@ type UpdateIssueProgressReq struct {
 	Progress  entities.Progress
 }
 
+// GetReportsReq input parameter to GetTotalReports
 type GetReportsReq struct {
 	Context ctxtg.Context
 	Tracker entities.Tracker
 	Date    int64
 }
 
+// GetReportsResp output parameter from GetTotalReports
 type GetReportsResp struct {
 	Total int64
 }
